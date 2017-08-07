7761becaa90edba637eafc01f7d2b4d38b282027
Shweta Shahane <shweta.shahane92@gmail.com>
2017-08-06 21:07:18 -0700

    location access working

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
  angular.module('myApp',['geolocation']).controller('mainCtrl',function($scope ,geolocation){
  	$scope.coords = geolocation.getLocation().then(function(data){
  		return {lat:data.coords.latitude,long:data.coords.longitude};
